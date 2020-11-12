f73dd6f05a4aaa3275a4eb363cba4713423c0e52
lgl <lk23286@gmail.com>
2020-11-12 08:25:13 +0100

    add labels and button functions

--- CleverSubtitle/View/ViewController.swift
+++ CleverSubtitle/View/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var Answer1ButtonLabel: UIButton!
+    @IBOutlet weak var Answer2ButtonLabel: UIButton!
+    @IBOutlet weak var Answer3ButtonLabel: UIButton!
+    @IBOutlet weak var GoodAnswerLabel: UILabel!
+    @IBOutlet weak var FaceLabel: UIImageView!
+    @IBOutlet weak var BadAnswerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Answer1Button(_ sender: UIButton) {
+    }
+    
+    @IBAction func Answer2Button(_ sender: UIButton) {
+    }
+    
+    @IBAction func Answer3Button(_ sender: UIButton) {
+    }
+    
 }
 
